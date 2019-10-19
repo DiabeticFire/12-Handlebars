fe4dd9fe10e5c30418b44e1dee3ca92723167407
Joseph <ejosephvito@gmail.com>
2019-10-19 15:25:48 -0400

    homework 12

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Bacon Burger');
+INSERT INTO burgers
+    (name)
+VALUES
+    ('Cheese Burger');
+INSERT INTO burgers
+    (name, sleepy)
+VALUES
+    ('Bacon Cheese Burger', true);
